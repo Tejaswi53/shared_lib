00e24b048412fd7dcaa86820ccf73ace91a2d31d
Tejaswi53 <169144624+Tejaswi53@users.noreply.github.com>
2024-12-12 23:30:48 +0530

    new code

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,6 @@
-def call() {
-    bat 'echo Tejaswi'
+def call(string repoUrl,string branch = 'main') {
+    checkout([$class: 'GitSCM',
+      branches: [[name: "*/${branch}"]],
+      userRemoteConfigs: [[url: repoUrl]]
+    ])
 }
